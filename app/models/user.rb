acf193da7e6a497b8d8339df3de827822bebb62e
taro.kirameki <bem0623@icloud.com>
2018-12-03 13:34:30 +0000

    user login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class Task < ApplicationRecord
   before_save { self.email.downcase! }
   validates :content, presence: true, length: { maximum: 255 }
-  validates :status, presence: true, length: { maximum: 10 }
+  validates :status, presence: true, length: { maximum: 10 },
+                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }  
   
   has_secure_password
-
-  has_many :microposts
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'toppages/index'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  root to: 'toppages#index'
  
@@ -11,6 +9,4 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
-  
-  resources :microposts, only: [:create, :destroy]
 end
